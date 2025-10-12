71662ee6a502719ae239355597dafe4f2a8bdc6a
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2025-10-13 04:32:18 +0800

    feat: implement side bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage.tsx";
+import AppPage from "./pages/AppPage.tsx";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage />} />
+                <Route path="/app" element={<AppPage />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/components/app/ScheduleCard.tsx
@@ -0,0 +1,37 @@
+﻿type Schedule = {
+    date: Date,
+    event: string
+}
+
+interface ScheduleCardProps {
+    schedule: Schedule[]
+}
+
+function ScheduleCard({ schedule = [] }: ScheduleCardProps) {
+    const formatter = new Intl.DateTimeFormat('en-US', {
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+    })
+    return (
+        <section id="upcomingEvents" className="rounded-2xl border-2 border-gray-200 space-y-3 p-3 w-2xs">
+            <h4 className="font-semibold text-lg">Upcoming Events</h4>
+
+            <div className="space-y-3">
+                {
+                    schedule.map((event, index) => (
+                        <div key={index}>
+                            <p className="text-gray-500">{formatter.format(event.date)}</p>
+                            <p>{event.event}</p>
+                        </div>
+                    ))
+                }
+            </div>
+        </section>
+    )
+}
+
+export type { Schedule }
+
+export default ScheduleCard
