d7a23540497e22b249cc24421a2c407293362d99
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2025-10-13 23:11:24 +0800

    feat: implement post component

--- src/pages/AppPage.tsx
+++ src/pages/AppPage.tsx
@@ -1,12 +1,17 @@
 ﻿import profilePicture from "/profile pic.png";
 import photo from "/Photo.png";
 import calendar from "/Calendar.png";
+import heart from "/Heart.png";
+import messageBubble from "/MessageBubble.png";
+import share from "/Share.png";
+import user from "/User.png";
 
 function AppPage() {
     return (
         <main className="flex-1 mx-auto flex xl:container xl:mx-auto">
             <div className="p-5 w-full space-y-5">
                 <AddPostForm/>
+                <Posts />
             </div>
         </main>
     )
@@ -40,6 +45,86 @@ function AddPostForm() {
                 </div>
             </div>
         </form>
+    )
+}
+
+interface User {
+    name: string,
+    course: string,
+    year: string | undefined,
+    profilePicture: string,
+    affiliation: string,
+}
+interface PostProps {
+    id: number,
+    user: User,
+    post: string,
+    image: string | undefined,
+    likes: number,
+    comments: number,
+    datePosted: Date,
+}
+function Post({ user, post, image, likes, comments, datePosted }: PostProps) {
+    return (
+        <div className="rounded-2xl border-2 p-4 space-y-7 border-gray-200">
+            <div className="flex space-x-3">
+                <div>
+                    <img src={user.profilePicture} alt={`${user.name} Profile Picture`} className="aspect-square rounded-full w-14"/>
+                </div>
+                <div>
+                    <div className="flex space-x-3">
+                        <p className="font-semibold text-xl">{user.name}</p>
+                        <span className="px-2 py-0.5 rounded bg-gray-100">{user.affiliation}</span>
+                    </div>
+                    {
+                        user.year
+                            ? <p>{`${user.course} • ${user.year} • ${datePosted.toDateString()}`}</p>
+                            : <p>{`${user.course} • ${datePosted.toDateString()}`}</p>
+                    }
+                </div>
+            </div>
+            <p>{post}</p>
+            { image && <div className="flex items-center w-full"><img src={image} alt="Post Image" className="rounded-xl w-full lg:w-1/2"/></div> }
+            <hr className="border-gray-200"/>
+            <div className="space-x-5 pb-3 px-3 flex">
+                <a href="" className="flex items-center space-x-4 rounded px-2 py-1 hover:bg-gray-100">
+                    <div><img src={heart} alt="Like" className="w-5"/></div>
+                    <span>{likes}</span>
+                </a>
+
+                <a href="" className="flex items-center space-x-4 rounded px-2 py-1 hover:bg-gray-100">
+                    <div><img src={messageBubble} alt="Comment" className="w-5"/></div>
+                    <span>{comments}</span>
+                </a>
+
+                <a href="" className="flex items-center space-x-4 rounded px-2 py-1 hover:bg-gray-100">
+                    <div><img src={share} alt="Share" className="w-5"/></div>
+                    <span>Share</span>
+                </a>
+
+            </div>
+        </div>
+    );
+}
+
+function Posts() {
+    const posts: PostProps[] = [
+        {
+            id: 1,
+            user: { name: "Renz Pagulayan", course: "Computer Science", profilePicture: user, affiliation: "Faculty" },
+            post: "I'm excited to share that I'm joining the Connectly team this summer! I'm looking forward to working with talented individuals and collaborating on exciting projects. Let's connect and make this a great summer!",
+            likes: 10,
+            comments: 5,
+            datePosted: new Date()
+        },
+    ];
+
+    return (
+        <div className="space-y-5">
+            {posts.map((post, _) => (
+                <Post key={post.id} {...post}/>
+            ))}
+        </div>
     )
 }
 
