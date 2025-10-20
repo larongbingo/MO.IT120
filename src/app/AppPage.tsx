c47397d5967e678b539f5f800ef4a2e45a1aa6d2
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2025-10-21 01:00:08 +0800

    feat: add sample photos from Pexels

--- src/app/AppPage.tsx
+++ src/app/AppPage.tsx
@@ -4,7 +4,7 @@ import calendar from "../assets/Calendar.png";
 import heart from "../assets/Heart.png";
 import messageBubble from "../assets/MessageBubble.png";
 import share from "../assets/Share.png";
-import user from "../assets/User.png";
+import userPhoto from "../assets/User.png";
 
 function AppPage() {
     return (
@@ -52,7 +52,7 @@ interface User {
     name: string,
     course: string,
     year?: string | undefined,
-    profilePicture: string,
+    profilePicture?: string | undefined,
     affiliation: string,
 }
 interface PostProps {
@@ -69,7 +69,7 @@ function Post({ user, post, image, likes, comments, datePosted }: PostProps) {
         <div className="rounded-2xl border-2 p-4 space-y-7 border-gray-200">
             <div className="flex space-x-3">
                 <div>
-                    <img src={user.profilePicture} alt={`${user.name} Profile Picture`} className="aspect-square rounded-full w-14"/>
+                    <img src={user.profilePicture ? user.profilePicture : userPhoto} alt={`${user.name} Profile Picture`} className="aspect-square rounded-full w-14"/>
                 </div>
                 <div>
                     <div className="flex space-x-3">
@@ -111,10 +111,59 @@ function Posts() {
     const posts: PostProps[] = [
         {
             id: 1,
-            user: { name: "Renz Pagulayan", course: "Computer Science", profilePicture: user, affiliation: "Faculty" },
+            user: { name: "Renz Pagulayan", course: "Computer Science", profilePicture: userPhoto, affiliation: "Faculty" },
             post: "I'm excited to share that I'm joining the Connectly team this summer! I'm looking forward to working with talented individuals and collaborating on exciting projects. Let's connect and make this a great summer!",
             likes: 10,
+            image: "https://images.pexels.com/photos/34364458/pexels-photo-34364458.jpeg?cs=srgb&dl=pexels-opticaltimeline-34364458.jpg&fm=jpg",
             comments: 5,
+            datePosted: new Date()
+        },
+        {
+            id: 2,
+            user: { name: "Sarah Martinez", course: "Business Administration", year: "3rd Year", profilePicture: "https://images.pexels.com/photos/3767392/pexels-photo-3767392.jpeg?cs=srgb&dl=pexels-olly-3767392.jpg&fm=jpg", affiliation: "Student" },
+            post: "Does anyone have notes from last week's Marketing 301 lecture? I had to miss class due to a medical appointment. Would really appreciate any help!",
+            likes: 23,
+            comments: 12,
+            datePosted: new Date()
+        },
+        {
+            id: 3,
+            user: { name: "Dr. James Chen", course: "Physics Department", profilePicture: "https://images.pexels.com/photos/762020/pexels-photo-762020.jpeg?cs=srgb&dl=pexels-olly-762020.jpg&fm=jpg", affiliation: "Faculty" },
+            post: "Reminder: Office hours this Thursday will be moved to Friday 2-4 PM due to the department meeting. See you all then!",
+            likes: 45,
+            comments: 8,
+            datePosted: new Date()
+        },
+        {
+            id: 4,
+            user: { name: "Alex Thompson", course: "Mechanical Engineering", year: "2nd Year", profilePicture: "https://images.pexels.com/photos/3777570/pexels-photo-3777570.jpeg?cs=srgb&dl=pexels-olly-3777570.jpg&fm=jpg", affiliation: "Student" },
+            post: "Just finished my first CAD project! It took me way longer than expected but I'm really proud of the result. Shoutout to the study group for helping me debug those dimension errors 🙌",
+            likes: 67,
+            comments: 19,
+            datePosted: new Date()
+        },
+        {
+            id: 5,
+            user: { name: "Emily Rodriguez", course: "Biology", year: "4th Year", profilePicture: userPhoto, affiliation: "Student" },
+            post: "Looking for lab partners for the Molecular Biology research project next semester. Interested in genetic engineering and CRISPR applications. DM me if you want to collaborate!",
+            likes: 34,
+            comments: 27,
+            datePosted: new Date()
+        },
+        {
+            id: 6,
+            user: { name: "Michael Park", course: "Graphic Design", year: "1st Year", affiliation: "Student" },
+            post: "Can anyone recommend good resources for learning Adobe Illustrator? I'm struggling with the pen tool and bezier curves. Any tutorials or tips would be amazing!",
+            likes: 89,
+            comments: 43,
+            datePosted: new Date()
+        },
+        {
+            id: 7,
+            user: { name: "Prof. Lisa Anderson", course: "English Literature", affiliation: "Faculty" },
+            post: "Excited to announce our department's upcoming Poetry Slam event on March 15th! Open to all students. Prizes for top 3 performers. Sign up at the Student Center by March 10th. Let's celebrate creativity together!",
+            likes: 156,
+            comments: 52,
             datePosted: new Date()
         },
     ];
