443f0dbf2a72a0d738cc0970ca67eaff10217099
Renz Christen Yeomer Pagulayan <rcyapagulayan@gmail.com>
2026-01-31 20:50:30 +0800

    feat: add google oauth

--- src/landing/LandingPage.tsx
+++ src/landing/LandingPage.tsx
@@ -7,18 +7,23 @@ import userLogo from "../assets/Users Logo.png";
 import lightningLogo from "../assets/Lightning Logo.png";
 import bookLogo from "../assets/Book Logo.png";
 import contactUsImage from "../assets/contact us section.png";
+import { useGoogleLogin } from "@react-oauth/google";
 
 function LandingPage() {
+    const login = useGoogleLogin({
+        onSuccess: tokenResponse => console.log(tokenResponse),
+        onError: (err) => console.log(err),
+        onNonOAuthError: err => console.log(err),
+    });
+    
     return (
         <>
             <Navbar homeRoute="/">
                 <ul className="flex items-center space-x-5">
                     <li>
-                        <Link to="/app">Sign In</Link>
-                    </li>
-                    <li>
-                        <Link to="/app" className="text-white bg-gray-800 hover:bg-gray-950 p-2 rounded">Get
-                            Started</Link>
+                        <a href="#" onClick={() => login()}>
+                            Sign In
+                        </a>
                     </li>
                 </ul>
             </Navbar>
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId='400269658902-uhahtjf051jct8ummfuecfnoc5i004ur.apps.googleusercontent.com'>
+      <App />
+    </GoogleOAuthProvider>
   </StrictMode>,
 )
